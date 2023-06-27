9fe17c2803e57c1cbb4fc027d153d9a9a5f48695
suman dulal <suman@zapp.today>
2023-06-27 11:37:01 +0545

    update

--- src/components/navbar/sidebar/index.js
+++ src/components/navbar/sidebar/index.js
@@ -1,55 +1,49 @@
-
-import React from 'react'
-import { Dashboard, Content, Sale, Licensed, Setting, Logout } from '@/svgicon'
+import React from "react";
+import { Dashboard, Content, Sale, Licensed, Setting, Logout } from "@/svgicon";
 const element = [
-    {
-        name: "dashboard",
-        icon: <Dashboard />,
-    },
-    {
-        name: "Content",
-        icon: <Content />,
-    },
-    {
-        name: "dashboard",
-        icon: <Dashboard />,
-    },
-    {
-        name: "Sale",
-        icon: <Sale />,
-    },
-    {
-        name: "Settings",
-        icon: <Setting />,
-    },
-    {
-        name: " Logout",
-        icon: <Logout />,
-    },
-]
+  {
+    name: "dashboard",
+    icon: <Dashboard />,
+  },
+  {
+    name: "Content",
+    icon: <Content />,
+  },
+  {
+    name: "dashboard",
+    icon: <Dashboard />,
+  },
+  {
+    name: "Sale",
+    icon: <Sale />,
+  },
+  {
+    name: "Settings",
+    icon: <Setting />,
+  },
+  {
+    name: " Logout",
+    icon: <Logout />,
+  },
+];
 
 const SideBar = () => {
-    return (
-        <div className=' '>
-            <ul className='text-white_300'>
-                {
-                    element.map((item, i) => {
-                        return (
-                            <li key={i} className=' capitalize flex text-[16px] font-semibold items-center gap-4 mb-10'>
-                                <span>{item.icon}</span> {item.name}
-                            </li>
+  return (
+    <div className=" ">
+      <ul className=" flex flex-wrap text-white_300">
+        {element.map((item, i) => {
+          return (
+            <li
+              key={i}
+              className=" capitalize flex text-[16px] font-semibold items-center gap-4 mb-10"
+            >
+              <span>{item.icon}</span> {item.name}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-
-                        )
-                    })
-                }
-
-
-            </ul>
-
-
-        </div>
-    )
-}
-
-export default SideBar
+export default SideBar;
