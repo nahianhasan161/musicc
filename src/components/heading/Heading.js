473d52543a925b6e933e3c36dc9e4dfbd7259f93
Al Nahian Hasan <nahianhasan161@gmail.com>
2023-07-19 17:46:07 +0600

    landing page cta section added

--- src/components/heading/Heading.js
+++ src/components/heading/Heading.js
@@ -4,11 +4,11 @@ export const Heading = ({ textColor, text, text2 , fontWeight }) => {
   return (
     <h1 className={` ${fontWeight} italic `}>
       <span
-        className={`text-white xl:text-[80px] sm:text-[60px] text-[50px] uppercase ${textColor} `}
+        className={`text-white sm:text-[80px] text-[60px] uppercase ${textColor} `}
       >
         {text}
       </span>
-      <span className={`xl:text-[80px] sm:text-[60px] text-[50px] uppercase text-[#D8EB55]`}>
+      <span className={`sm:text-[80px]  text-[60px] uppercase text-[#D8EB55]`}>
         {text2}
       </span>
     </h1>
--- src/components/home/HeroHome.js
+++ src/components/home/HeroHome.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Heading } from "../heading/Heading";
 import { SearchHome } from "../search/Search";
 import Image from "next/image";
+import downIcon from "/src/svgicon/keyboard_arrow_down.svg"
+import { Button } from "../button";
+
+import {FaArrowDown} from "react-icons/fa"
 const HomeHero = () => {
   return (
     <section className="bg-black ml-2">
-      <div className="flex max-w-screen-xl px-4 sm:px-[5rem]  py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div className="flex max-w-screen-xl px-4 sm:px-[5rem]  py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
         {/* increased the font */}
         <div className="flex flex-col  leading-[60px]">
           <Heading text="WE" fontWeight="font-bold" />
@@ -17,12 +21,68 @@ const HomeHero = () => {
         </div>
        
 
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex ml-auto w-[30rem]">
+        {/*   <img
             src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
             alt="mockup"
-          />
-        </div>
+          /> */}
+         {/*  color: #E6E6E6;
+font-family: AmsiProNarw-BoldItalic;
+font-size: 20px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+text-transform: uppercase; */}
+           <div className="text-[20px] font-bold uppercase text-white space-y-8">
+            <div className="flex gap-3 items-center justify-between  text-[20px] pr-10 ">
+
+           <Button
+                className={
+                  "italic rounded-full  font-semibold text-[20px]  sm:text-[24px] px-4  sm:px-4  py-3 sm:py-3 flex items-center gap-5"
+                }
+                >
+                GENRE<FaArrowDown/>
+              </Button>
+            <p className="">MOOD</p>
+            <p className="">BPM</p>
+           </div>
+           <div className="flex justify-between items-center">
+            <div className="flex flex-col gap-5">
+          
+            <h1 className="text-4xl font-bold italic ">50+</h1>
+            <p className="font-light text-[#C3C3C3] text-base">
+            A community of more than 50+ music producers.
+            </p>
+            </div>
+            <div className="flex flex-col gap-5">
+
+         
+            <h1 className="text-4xl font-bold italic ">1000+</h1>
+            <p className="font-light text-[#C3C3C3] text-base">And a catalogue of over 1000+ beats which updates, daily!</p>
+            </div>
+           </div>
+           {/* color: #1C1C1C;
+font-family: AmsiProNarw-SemiBoldItalic;
+font-size: 20px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+text-transform: uppercase; */}
+                <Button
+                className={
+                  " italic rounded-full w-full  font-semibold text-[20px]  sm:text-[24px] px-4  sm:px-8  py-3 sm:py-3 flex items-center gap-5 justify-center"
+                }
+                >
+                Explore 
+                </Button>
+                </div>
+        </div> 
+       {/*  <section>
+          <div>
+            <p>Genre <label></label> </p>
+          </div>
+        </section> */}
+        
       </div>
     </section>
   );
